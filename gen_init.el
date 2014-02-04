3365e86e037b76c973e40653a1be7a08e1d03f1c
Eagleeye777 <eagleeye777@hotmail.de>
2014-02-04 15:58:38 +0100

    Fixing copy paste bug

--- gen_init.el
+++ gen_init.el
@@ -15,14 +15,15 @@
 ;;moved from better defaults package
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
-  (setq x-select-enable-clipboard t
-        x-select-enable-primary t
-        save-interprogram-paste-before-kill t
-        apropos-do-all t
-        mouse-yank-at-point t
-        save-place-file (concat user-emacs-directory "places")
-        backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                                 "backups"))))
+  (setq
+   x-select-enable-clipboard t
+   ;; x-select-enable-primary t
+   save-interprogram-paste-before-kill t
+   apropos-do-all t
+   mouse-yank-at-point t
+   save-place-file (concat user-emacs-directory "places")
+   backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                            "backups"))))
 ;; Keep mode line tidy
 (autoload 'diminish "" "" t)
 
